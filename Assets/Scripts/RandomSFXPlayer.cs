adbc9a29470644df80479bb204dc01ae9ec91c46
ChipironWeaver <lowen.tisserand@gmail.com>
2026-04-12 07:43:31 +0200

    SFX Import

--- Assets/Scripts/RandomSFXPlayer.cs
+++ Assets/Scripts/RandomSFXPlayer.cs
@@ -1,12 +1,45 @@
+using System;
 using System.Collections.Generic;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 public class RandomSfxPlayer : MonoBehaviour
 {
-    [SerializeField] private List<AudioClip> _clips;
-
-    public void Play()
+    [SerializeField] private List<AudioClipName> _clips;
+    public static RandomSfxPlayer Instance { get; private set; }
+    [Serializable]
+    public class AudioClipName
     {
-        AudioSource.PlayClipAtPoint(_clips[Random.Range(0, _clips.Count)], Vector2.zero);
+        public string name;
+        public List<AudioClip> clips;
+
+        public bool CheckName(string name)
+        {
+            return name == name;
+        }
+    }
+    
+    public void Play(string clipName)
+    {
+        foreach (AudioClipName clip in _clips)
+        {
+            if (clip.CheckName(clipName))
+            {
+                AudioSource.PlayClipAtPoint(clip.clips[Random.Range(0, _clips.Count)], transform.position);
+                return;
+            }
+        }
+    }
+
+    void Singleton()
+    {
+        if (Instance !=null && Instance != this)
+        {
+            Destroy(this);
+        }
+        else
+        {
+            Instance = this;
+        }
     }
 }

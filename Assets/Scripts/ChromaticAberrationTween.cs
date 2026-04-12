bddfb3c28e4e739de65fb6a68bde94df0926186f
foCs <focs.cc.1987@gmail.com>
2026-04-12 12:58:49 +0200

    Working chromatic aberration

--- Assets/Scripts/ChromaticAberrationTween.cs
+++ Assets/Scripts/ChromaticAberrationTween.cs
@@ -3,19 +3,22 @@ using DG.Tweening;
 
 public class ChromaticAberrationTween : MonoBehaviour
 {
+    [SerializeField] Material CrtMat;
     [SerializeField] float baseBlur = .0015f;
 
     [Header("Hit")]
     [SerializeField] float blurHit = .01f;
-    [SerializeField] float durationHit;
+    [SerializeField] float durationHit = .5f;
 
     [Header("Die")]
-    [SerializeField] AnimationCurve blurAnimDie;
+    [SerializeField] float blurDie;
     [SerializeField] float durationDie;
 
     [Header("Next Wave")]
-    [SerializeField] AnimationCurve blurAnimWave;
+    [SerializeField] float blurWave;
     [SerializeField] float durationWave;
+
+    Sequence chromaticSequence;
 
     void OnEnable()
     {
@@ -26,23 +29,52 @@ public class ChromaticAberrationTween : MonoBehaviour
 
     void OnDisable()
     {
-        Actions.OnPlayerHit += HitTween;
-        Actions.OnPlayerDie += DieTween;
-        Actions.OnNewWave += WaveTween;
+        Actions.OnPlayerHit -= HitTween;
+        Actions.OnPlayerDie -= DieTween;
+        Actions.OnNewWave -= WaveTween;
     }
 
-    void HitTween(float temp)
+    void HitTween(float health)
     {
-        
+        if (health <= 0) return;
+
+        CrtMat.SetInt("_UseChromaticAberration", 1);
+        CrtMat.SetFloat("_BlurOffset", blurHit);
+
+        chromaticSequence = DOTween.Sequence();
+        chromaticSequence.AppendInterval(durationHit);
+        chromaticSequence.OnComplete(() => 
+        { 
+            CrtMat.SetInt("_UseChromaticAberration", 0);
+            CrtMat.SetFloat("_BlurOffset", baseBlur);
+        });
     }
 
     void DieTween()
     {
-        
+        CrtMat.SetInt("_UseChromaticAberration", 1);
+        CrtMat.SetFloat("_BlurOffset", blurDie);
+
+        chromaticSequence = DOTween.Sequence();
+        chromaticSequence.AppendInterval(durationDie);
+        chromaticSequence.OnComplete(() => 
+        { 
+            CrtMat.SetInt("_UseChromaticAberration", 0);
+            CrtMat.SetFloat("_BlurOffset", baseBlur);
+        });
     }
 
     void WaveTween()
     {
-        
+        CrtMat.SetInt("_UseChromaticAberration", 1);
+        CrtMat.SetFloat("_BlurOffset", blurWave);
+
+        chromaticSequence = DOTween.Sequence();
+        chromaticSequence.AppendInterval(durationWave);
+        chromaticSequence.OnComplete(() => 
+        { 
+            CrtMat.SetInt("_UseChromaticAberration", 0);
+            CrtMat.SetFloat("_BlurOffset", baseBlur);
+        });
     }
 }

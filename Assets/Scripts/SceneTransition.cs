37232aa6e3b032a8ae01d79efe6058d13cf831f6
ChipironWeaver <lowen.tisserand@gmail.com>
2026-04-14 00:18:13 +0200

    WIP Scene Fade out done, Scene Fade In needed

--- Assets/Scripts/SceneTransition.cs
+++ Assets/Scripts/SceneTransition.cs
@@ -1,20 +1,19 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using DG.Tweening;
 using NaughtyAttributes;
-using NUnit.Framework;
-using Unity.VisualScripting;
 using UnityEngine;
-using UnityEngine.Rendering;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 using Random = UnityEngine.Random;
 
 public class SceneTransition : MonoBehaviour
 {
-    [SerializeField] private float _fadeInDuration = 0.5f;
-    [SerializeField] private float _minimunLoadingWait = 1f;
+    [SerializeField] private Ease _easing;
     [SerializeField] private float _fadeOutDuration = 0.5f;
+    [SerializeField] private float _loadingWait = 1f;
+    [SerializeField] private float _fadeInDuration = 0.5f;
     
     [SerializeField] private CanvasGroup _canvasGroup;
     [SerializeField] private Image _background;
@@ -29,9 +28,14 @@ public class SceneTransition : MonoBehaviour
         public void FadeOut(string sceneName)
     {
         SceneTransitions sceneTransitions = _sceneTransitions[Random.Range(0, _sceneTransitions.Count)];
-        Color color = sceneTransitions.colors[Random.Range(0, sceneTransitions.colors.Count)];
+        _background.color = sceneTransitions.colors[Random.Range(0, sceneTransitions.colors.Count)];
         sceneTransitions.animations[Random.Range(0,sceneTransitions.animations.Count)].gameObject.SetActive(true);
 
+        Sequence fadeOut = DOTween.Sequence();
+        fadeOut.Append(_canvasGroup.DOFade(1, _fadeOutDuration));
+        fadeOut.AppendInterval(_loadingWait);
+        fadeOut.AppendCallback(() => SceneManager.LoadSceneAsync(sceneName));
+        print("start load scene");
         //StartCoroutine(FadeCoroutine(color, sceneName));
     }
 }
